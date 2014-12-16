fef92199bca58f393df0699d0c033dd33cc75559
jtimberman <joshua@chef.io>
2014-12-16 12:15:16 -0700

    In Chef 12, prefer config.rb to knife.rb
    The configuration is to be used by both `knife.rb` (in future Chef
    Server use case), and `chef-client` with local mode.

--- .chef/config.rb
+++ .chef/config.rb
@@ -1,6 +1,9 @@
 current_dir = File.dirname(__FILE__)
+json_attribs_file = File.join(current_dir, '..', 'dna.json')
 # https://github.com/opscode/chef/issues/2449
 chef_repo_path File.join(current_dir, '..')
+
+json_attribs json_attribs_file if json_attribs_file
 
 cookbook_path [
   File.join(current_dir, '..', 'cookbooks'),
