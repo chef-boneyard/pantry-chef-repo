d1a3c33d241427ab20d2f365b219354cf0f5b40f
jtimberman <joshua@chef.io>
2014-12-16 13:31:30 -0700

    actually use a conditional that makes sense

--- .chef/config.rb
+++ .chef/config.rb
@@ -3,7 +3,7 @@ json_attribs_file = File.join(current_dir, '..', 'dna.json')
 # https://github.com/opscode/chef/issues/2449
 chef_repo_path File.join(current_dir, '..')
 
-json_attribs json_attribs_file if json_attribs_file
+json_attribs json_attribs_file if File.exist?(json_attribs_file)
 
 cookbook_path [
   File.join(current_dir, '..', 'cookbooks'),
